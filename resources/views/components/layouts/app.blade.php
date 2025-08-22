0022147e0faaa12e6c690a437c687f700db52c3c
Emil Maulana <emilmaulana10@gmail.com>
2025-08-22 17:32:51 +0700

    `Commented out @vite('resources/css/app.css') in app.blade.php`

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <title>{{ $title ?? 'Wanara Link' }}</title>
 
     <!-- Tailwind CSS CDN -->
-    @vite('resources/css/app.css')
+    {{-- @vite('resources/css/app.css') --}}
     <!-- Font Awesome untuk ikon -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
