14038cb69c5c4c66f2b5a256ce2b00309b817faf
Emil Maulana <emilmaulana10@gmail.com>
2025-08-22 17:35:50 +0700

    `Added Tailwind CSS CDN and Font Awesome stylesheet to app.blade.php`

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <!-- Tailwind CSS CDN -->
     {{-- @vite('resources/css/app.css') --}}
     <!-- Font Awesome untuk ikon -->
+    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
 </head>
