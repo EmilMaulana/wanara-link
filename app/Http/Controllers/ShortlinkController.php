38c4664dbc29400d598a4f618b12c35f35441a0b
Emil Maulana <emilmaulana10@gmail.com>
2025-09-03 01:28:32 +0700

    `Fixed typo in namespace and model name in ShortlinkController`

--- app/Http/Controllers/ShortlinkController.php
+++ app/Http/Controllers/ShortlinkController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\ShortLink;
+use App\Models\Shortlink;
 
 class ShortlinkController extends Controller
 {
     public function redirect($alias)
     {
-        $shortlink = ShortLink::where('alias', $alias)->first();
+        $shortlink = Shortlink::where('alias', $alias)->first();
 
         if (! $shortlink) {
             abort(404, 'Link tidak ditemukan');
